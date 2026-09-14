bb290eb9125df76961992ea66145b20f0346a82c
maynichevrd.28 <roman.mainichev@gmail.com>
2026-09-14 17:23:46 +0300

    Sun

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,35 @@
-public class Main {
-    public static void main(String[] args) {
-               //System.out.printf("Hello and welcome!");
+import java.awt.*;
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+
+public class Main extends Frame {
+
+    public Main()
+    {
+        setTitle("first drawing");
+        setSize(800,640);
+        setVisible(true);
+    }
+    public void paint(Graphics g)
+    {
+        super.paint(g);
+        Graphics2D isa = (Graphics2D) g;
+        g.setColor(Color.YELLOW);
+        g.fillOval(100,100, 50, 50);
+
+        g.drawLine(125,75, 125, 90);
+        g.drawLine(125,155, 125, 170);
+        g.drawLine(95,125, 80, 125);
+        g.drawLine(155,125, 170, 125);
+        g.drawLine(104,104, 92, 92);
+        g.drawLine(145,104, 157, 92);
+        g.drawLine(104,146, 92, 157);
+        g.drawLine(145,145, 157, 157);
+
+
+    }
+
+    public static void main(String[] args) {
+        //System.out.println("test");
+        new Main();
     }
 }
